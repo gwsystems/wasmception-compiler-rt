gwsystems/wasmception-compiler-rt

commit d04814f040e2ba045744d3b2398c3d09bc6ae0a3

Reland "Make TracePcGuardController linker-initialized"
It was always intended to be.

Patch By: mcgrathr

Differential Revision: https://reviews.llvm.org/D41513

git-svn-id: http://llvm.org/svn/llvm-project/compiler-rt/trunk@322991 91177308-0d34-0410-b5e6-96231b3b80d8

lib/sanitizer_common/sanitizer_coverage_fuchsia.cc
@@ -49,7 +49,7 @@ constexpr const char kSancovSinkName[] = "sancov";
 
 // Collects trace-pc guard coverage.
 // This class relies on zero-initialization.
-class TracePcGuardController {
+class TracePcGuardController final {
  public:
   // For each PC location being tracked, there is a u32 reserved in global
   // data called the "guard".  At startup, we assign each guard slot a
@@ -113,11 +113,11 @@ class TracePcGuardController {
   // We can always spare the 32G of address space.
   static constexpr size_t MappingSize = sizeof(uptr) << 32;
 
-  BlockingMutex setup_lock_;
-  uptr *array_;
-  u32 next_index_;
-  zx_handle_t vmo_;
-  char vmo_name_[ZX_MAX_NAME_LEN];
+  BlockingMutex setup_lock_ = {LINKER_INITIALIZED};
+  uptr *array_ = nullptr;
+  u32 next_index_ = 0;
+  zx_handle_t vmo _ = {};
+  char vmo_name_[ZX_MAX_NAME_LEN] = {};
 
   size_t DataSize() const { return next_index_ * sizeof(uintptr_t); }
 

commit 16958b6b8d395995bab5dee64dea7572c9a605b9

Reland "Fix syntax error introduced in r322991"
This triggers compiler error when building sanitizers for Fuchsia.

Differential Revision: https://reviews.llvm.org/D42328

git-svn-id: http://llvm.org/svn/llvm-project/compiler-rt/trunk@323029 91177308-0d34-0410-b5e6-96231b3b80d8

lib/sanitizer_common/sanitizer_coverage_fuchsia.cc
@@ -113,10 +113,10 @@ class TracePcGuardController final {
   // We can always spare the 32G of address space.
   static constexpr size_t MappingSize = sizeof(uptr) << 32;
 
-  BlockingMutex setup_lock_ = {LINKER_INITIALIZED};
+  BlockingMutex setup_lock_ = BlockingMutex(LINKER_INITIALIZED);
   uptr *array_ = nullptr;
   u32 next_index_ = 0;
-  zx_handle_t vmo _ = {};
+  zx_handle_t vmo_ = {};
   char vmo_name_[ZX_MAX_NAME_LEN] = {};
 
   size_t DataSize() const { return next_index_ * sizeof(uintptr_t); }